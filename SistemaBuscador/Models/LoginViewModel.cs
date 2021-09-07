4c3ebd9846c755e47d2d1a8c794fcf5e1bb299d1
FELIPE GONZALO GUTIERREZ MEDINA <FELIPE.GUTIERREZME@correoaiep.cl>
2021-09-07 18:40:20 -0300

    cambio 18:40

---
+++ SistemaBuscador/Models/LoginViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SistemaBuscador.Models
+{
+    public class LoginViewModel
+    {
+        public string Ususario { get; set; }
+        [Display(Name = "Contraseña")]
+
+        public string Password { get; set; }
+       
+    }
+}
